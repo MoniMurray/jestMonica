48d57ade69d3ef7851e7b460d3450441bf17a9b0
Monica Murray <Monica.rickerby@gmail.com>
2023-01-16 21:42:06 +0000

    Use beforeEach() and Node.fs module to read theentire  contents of our HTML file within Jest

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -1,7 +1,12 @@
 const buttonclick = require("../button");
 
 beforeEach(() => {
-    document.body.innerHTML = "<p id='par'></p>";
+    // document.body.innerHTML = "<p id='par'></p>";
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
 });
 
 describe("DOM test", () => {
@@ -9,4 +14,7 @@ describe("DOM test", () => {
         buttonclick();
         expect(document.getElementById("par").innerHTML).toEqual("You clicked!");
     });
+    test("expects h1 to exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
+    })
 });
